ae07892e8db734388638a6db3181897b158f6515
Thenuwara <pramukhapoo@gmail.com>
2024-07-14 01:16:00 +0530

    Done some parts

--- app/src/main/java/com/example/doitnow/fragments/SignupFragment.kt
+++ app/src/main/java/com/example/doitnow/fragments/SignupFragment.kt
@@ -5,56 +5,67 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.doitnow.R
+import com.example.doitnow.databinding.FragmentSignupBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SignupFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignupFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var auth:FirebaseAuth
+    private lateinit var navControl:NavController
+    private lateinit var binding:FragmentSignupBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentSignupBinding.inflate(inflater, container, false)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignupFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignupFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+        registerEvents()
+    }
+
+    private fun init(view:View){
+        navControl = Navigation.findNavController(view)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun registerEvents(){
+
+        binding.authTextView.setOnClickListener {
+            navControl.navigate(R.id.action_signupFragment_to_loginFragment)
+        }
+        binding.nxtbtn.setOnClickListener {
+            val email = binding.useremail.text.toString()
+            val pass = binding.userpassword.text.toString()
+            val repass = binding.userrepassword.text.toString()
+
+            if(email.isNotEmpty() && pass.isNotEmpty() && repass.isNotEmpty()){
+                if (pass == repass){
+                    auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(
+                        OnCompleteListener {
+                            if(it.isSuccessful){
+                                Toast.makeText( context, "Registered Successfully", Toast.LENGTH_SHORT).show()
+                                navControl.navigate(R.id.action_signupFragment_to_homeFragment)
+                            }else{
+                                Toast.makeText( context, it.exception?.message, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    )
                 }
             }
+
+        }
     }
+
 }
